782217b1f89b33c7952761ed153add73e01bc482
Sean <sean@letsjoggle.com>
2022-05-16 00:32:14 -0400

    Set Menu Color

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import './App.css';
 
 import { AppBar, Toolbar, IconButton, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import { green } from '@mui/material/colors';
 
 function App() {
   return (
     <div className="App">
-      <AppBar position="static">
+      <AppBar position="static" sx={{ bgcolor: "darkgreen" }}>
         <Toolbar variant="dense">
           <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" color="inherit" component="div">
-            Photos
+            Foosball Tournament Referee
           </Typography>
         </Toolbar>
       </AppBar>
